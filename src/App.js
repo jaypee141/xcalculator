1b69b47566faeb88b81c0beaabd3bfe3e1415cc7
unknown <jaypee141@gmail.com>
2024-02-24 18:59:16 +0530

    get it

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from "react";
+
+function App() {
+  const[number1, getNumber] = useState('');
+  const [output, setOutput] = useState(0);
+  const [stat, setStat] =useState(false);
+  
+  const clickHandle = (e) => {
+    setStat(false);
+    getNumber(number1 + e.target.value);
+
+  }
+  console.log("hiiiiiiii", number1);
+  const clickHandleEqual = () => {
+    setStat(true)
+    // console.log("njjnnn", parseFloat(number1))
+    if(number1){
+    setOutput(eval(number1).toString());
+    }else{
+      setOutput("Error");
+    }
+  }
+   
+ 
+
+
+  return (
+    <div className="App">
+      <h1>React Calculator</h1>
+      <input type="text" value={stat ? output : number1} className='inpu'></input>
+      {/* <h1>{output}</h1> */}
+      <br/>
+      <input type="button" value="7" className='butto' onClick={clickHandle} />
+      <input type="button" value="8" className='butto'onClick={clickHandle} />
+      <input type="button" value="9" className='butto' onClick={clickHandle} />
+      <input type="button" value="+" className='butto' onClick={clickHandle} />
+      <br/>
+      <input type="button" value="4" className='butto' onClick={clickHandle} />
+      <input type="button" value="5" className='butto' onClick={clickHandle} />
+      <input type="button" value="6" className='butto' onClick={clickHandle} />
+      <input type="button" value="-" className='butto' onClick={clickHandle} />
+      <br/>
+      <input type="button" value="1" className='butto' onClick={clickHandle} />
+      <input type="button" value="2" className='butto' onClick={clickHandle} />
+      <input type="button" value="3" className='butto' onClick={clickHandle} />
+      <input type="button" value="*" className='butto' onClick={clickHandle} />
+      <br/>
+      <input type="button" value="C" className='buttoC' onClick={() => getNumber('')} />
+      <input type="button" value="0" className='butto' onClick={clickHandle} />
+      <input type="button" value="=" className='buttoEqual' onClick={clickHandleEqual} />
+      <input type="button" value="/" className='butto' onClick={clickHandle} />
+
+
+    </div>
+  );
+}
+
+export default App;
