dd4fa6d5b308f8524e310f94241cb2235e73c621
unknown <jaypee141@gmail.com>
2024-02-27 15:29:28 +0530

    gtht

--- src/App.js
+++ src/App.js
@@ -3,62 +3,54 @@ import './App.css';
 import React, { useState } from "react";
 
 function App() {
-  const[number1, getNumber] = useState('');
+  const [number1, setNumber] = useState('');
   const [output, setOutput] = useState(null);
-  // const [stat, setStat] =useState(false);
-  
-  const clickHandle = (e) => {
-    // setStat(false);
-    getNumber(number1 + e.target.value);
 
-  }
-  console.log("hiiiiiiii", number1);
+  const clickHandle = (e) => {
+    setNumber(number1 + e.target.value);
+  };
+
   const clickHandleEqual = () => {
-    // setStat(true)
-    // console.log("njjnnn", parseFloat(number1))
-    if(number1){
-    setOutput(eval(number1).toString());
-    }else{
+    if (number1) {
+      setOutput(eval(number1).toString());
+    } else {
       setOutput("Error");
     }
-  }
+  };
 
-  const callData = (() =>{
+  const clearData = () => {
     setOutput(null);
-    getNumber(' ');  
-  }
-  )
-   
- 
-
+    setNumber('');
+  };
 
   return (
     <div className="App">
       <h1>React Calculator</h1>
-      <input type="text" value={number1} className='inpu'></input>
-      <h1 style={{display: "block"}}>{output}</h1>
-      <br/>
+      {/* Updated class name here */}
+      <input type="text" value={number1} className='inpu' readOnly />
+      <h1 style={{ display: "block" }}>{output}</h1>
+      <br />
+      {/* Updated class names here */}
       <input type="button" value="7" className='butto' onClick={clickHandle} />
-      <input type="button" value="8" className='butto'onClick={clickHandle} />
+      <input type="button" value="8" className='butto' onClick={clickHandle} />
       <input type="button" value="9" className='butto' onClick={clickHandle} />
       <input type="button" value="+" className='butto' onClick={clickHandle} />
-      <br/>
+      <br />
       <input type="button" value="4" className='butto' onClick={clickHandle} />
       <input type="button" value="5" className='butto' onClick={clickHandle} />
       <input type="button" value="6" className='butto' onClick={clickHandle} />
       <input type="button" value="-" className='butto' onClick={clickHandle} />
-      <br/>
+      <br />
       <input type="button" value="1" className='butto' onClick={clickHandle} />
       <input type="button" value="2" className='butto' onClick={clickHandle} />
       <input type="button" value="3" className='butto' onClick={clickHandle} />
       <input type="button" value="*" className='butto' onClick={clickHandle} />
-      <br/>
-      <input type="button" value="C" className='buttoC' onClick={callData} />
+      <br />
+      <input type="button" value="C" className='buttoC' onClick={clearData} />
       <input type="button" value="0" className='butto' onClick={clickHandle} />
+      {/* Updated class name here */}
       <input type="button" value="=" className='buttoEqual' onClick={clickHandleEqual} />
       <input type="button" value="/" className='butto' onClick={clickHandle} />
-
-
     </div>
   );
 }
